423a6db10ee4e042e58e5e0812c23d5485913044
BuildTools <unconfigured@null.spigotmc.org>
2020-07-28 10:12:41 -0700

    initial commit

---
+++ jstris.py
@@ -0,0 +1,201 @@
+import numpy as np
+from pynput.keyboard import Key, Controller as kController
+from pynput import keyboard
+import time
+import cv2
+from mss import mss
+import os
+from tkinter import Tk
+import tetris_logic
+
+#print(tetris_logic.mult_py(np.array([[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 2.0, 3.0, 4.0, 5.0]])))
+
+monitor = Tk()
+
+#time.sleep(3) #For testing 
+
+#global variables
+box_unit = 24
+
+#define pieces:
+#   0: I block (cyan), 1: O block (yellow), 2: T block (pink), 3: S block (green), 
+#   4: Z block (red), 5: J block (blue), 6: L block (orang)
+I_bgr = [215, 155, 15]
+O_bgr = [2,159,227]
+T_bgr = [138, 41, 175]
+S_bgr = [1, 177, 89]
+Z_bgr = [55, 15, 215]
+J_bgr = [198, 65, 33]
+L_bgr = [2, 91, 227]
+Blank_BGR = [0, 0, 0]
+piece_bgr_values = [I_bgr, O_bgr, T_bgr, S_bgr, Z_bgr, J_bgr, L_bgr]
+upcoming_pieces = [-1] * 5
+saved_piece = -1
+
+#colors:
+gray_bgr = [106, 106, 106]
+black_bgr = [0, 0, 0]
+message_box_bgr = [28, 28, 28]
+
+screen_width = monitor.winfo_screenwidth()
+screen_height = monitor.winfo_screenheight()
+
+bounding_box = {'top': 0, 'left': 0, 'width': screen_width, 'height': screen_height}
+sct = mss()
+
+game_over = False
+
+keyboardCont = kController()
+
+fullscr_img = cv2.cvtColor(np.array(sct.grab(bounding_box)), cv2.COLOR_BGRA2BGR)
+#cv2.imwrite("test.png", fullscr_img)
+
+corner_img = cv2.imread(os.path.join("Res", "corner.png"), cv2.IMREAD_COLOR)
+
+gg_img = cv2.imread(os.path.join("Res", "gg.png"), cv2.IMREAD_COLOR)
+gg_alt_img = cv2.imread(os.path.join("Res", "gg_alt.png"), cv2.IMREAD_COLOR)
+
+corner_coords = tetris_logic.get_corner_coords(fullscr_img, corner_img, box_unit)
+print(corner_coords)
+
+game_box = {'top': corner_coords[0], 'left': corner_coords[1], 'width': box_unit * 14, 'height': box_unit * 20}
+
+game_img = cv2.cvtColor(np.array(sct.grab(game_box)), cv2.COLOR_BGRA2BGR)
+
+upcoming_pieces = [0, 0, 0, 0, 0]
+ 
+#checks the top level for any gray squares frite
+def check_game_over():
+    global game_over
+    global corner_coords
+    global game_img
+    for square in game_img[round(box_unit/2), round(box_unit/2): box_unit * 10 + round(box_unit/2): box_unit]:
+        if array_diff(square, gray_bgr) == 0:
+            print("GG took an L")
+            game_over = True
+            return True
+    game_over = False
+    return False
+
+def check_game_starting():
+    global message_box_bgr
+    global box_unit
+    global game_img
+    if array_diff(game_img[box_unit * 12, 0], message_box_bgr) == 0:
+        return True
+    else:
+        return False
+
+
+def array_diff(arr1, arr2):
+    if len(arr1) != len(arr2):
+        raise ValueError("Smh. Arrays must be the same size")
+    Error = 0
+    for n in range(len(arr1)):
+        Error += (arr1[n] - arr2[n])*2
+    return Error/len(arr1)
+
+# The Method that was not Used
+def get_upcoming_pieces():
+    pieces = []
+    for i in range(5):
+        piece = tetris_logic.identify_piece(fullscr_img[corner_coords[1] + round(box_unit * (1.5 + i * 3)), corner_coords[0] + round(box_unit * 12.5)])
+        if piece == 404:
+            piece = tetris_logic.identify_piece(fullscr_img[corner_coords[1] + round(box_unit * (2.5 + i * 3)), corner_coords[0] + round(box_unit * 12.5)])
+            if piece == 404:
+                raise RuntimeError("Smh. Color wrong or grabbing from wrong place")
+            else:
+                pieces.append(piece)
+        else:
+            pieces.append(piece)
+    return pieces
+
+keyboard_delay = 0.016000001
+#keyboard_delay = 0.02
+def make_move(move):
+    if move[2] == True:
+        keyboardCont.press('c')
+        keyboardCont.release('c')
+        time.sleep(keyboard_delay)
+    for i in range(0, move[0]):
+        keyboardCont.press(Key.up)
+        keyboardCont.release(Key.up)
+        time.sleep(keyboard_delay)
+    if move[1] < 0:
+        for i in range(0, abs(move[1])):
+            keyboardCont.press(Key.left)
+            keyboardCont.release(Key.left)
+            time.sleep(keyboard_delay)
+    else:
+        for i in range(0, abs(move[1])):
+            keyboardCont.press(Key.right)
+            keyboardCont.release(Key.right)
+            time.sleep(keyboard_delay)
+    keyboardCont.press(Key.space)
+    keyboardCont.release(Key.space)
+    time.sleep(keyboard_delay)
+
+def get_falling_piece():
+    global game_img
+    global box_unit
+    piece = tetris_logic.identify_piece(game_img[0, box_unit * 5])
+    if piece != 404:
+        return piece
+
+#make new pic
+def update_pic():
+    global bounding_box
+    global keyboardCont
+    global game_img
+    global upcoming_pieces
+    global saved_piece
+    game_img = cv2.cvtColor(np.array(sct.grab(game_box)), cv2.COLOR_BGRA2BGR)
+    move = tetris_logic.get_next_move(game_img, 2)
+    make_move(move)
+    time.sleep(0)
+    
+#This checks if the game has started at all, or if the previous game didn't end
+while check_game_starting() == False:
+    game_img = cv2.cvtColor(np.array(sct.grab(game_box)), cv2.COLOR_BGRA2BGR)
+
+#If the game is starting (display ready/go banner), takes screenshot of all upcoming pieces
+print("Game Starting...")
+#game_img = cv2.cvtColor(np.array(sct.grab(game_box)), cv2.COLOR_BGRA2BGR)
+#upcoming_pieces = tetris_logic.get_upcoming_pieces(fullscr_img, box_unit, corner_coords)
+
+while check_game_starting():
+    game_img = cv2.cvtColor(np.array(sct.grab(game_box)), cv2.COLOR_BGRA2BGR)
+print("Game Start!")
+
+while get_falling_piece() == None:
+    game_img = cv2.cvtColor(np.array(sct.grab(game_box)), cv2.COLOR_BGRA2BGR)
+
+stored_piece = False
+
+if stored_piece:
+    tetris_logic.set_stored_piece(get_falling_piece())
+    upcoming_pieces = tetris_logic.get_upcoming_pieces(game_img, box_unit)
+
+else:
+    upcoming_pieces = [get_falling_piece()] + tetris_logic.get_upcoming_pieces(game_img, box_unit)
+tetris_logic.set_upcoming_pieces(upcoming_pieces)
+
+if stored_piece:
+    keyboardCont.press('c')
+    keyboardCont.release('c')
+time.sleep(0.5)
+
+print(upcoming_pieces)
+
+while check_game_over() == False:
+    update_pic()
+    
+
+
+
+#heights = [0] * 10
+
+#next box 28 from board
+
+#keyboardCont.press('a')
+#keyboardCont.release('a')
---
+++ tetris_logic/src/tests.rs
@@ -0,0 +1,465 @@
+use rayon::prelude::*;
+use ndarray::prelude::*;
+use ndarray::stack;
+use pyo3::exceptions;
+use std::sync::Arc;
+use std::fmt::Display;
+use std::fmt;
+use std::sync::Mutex;
+use std::thread;
+use std::sync::atomic::{AtomicI32, AtomicUsize, AtomicU8, Ordering};
+
+
+lazy_static! {
+    static ref I_PIECE: Arc<Piece> = Arc::new(Piece {
+        id: 0,
+        bgr: [215, 155, 15],
+        orientations: vec![(3, array![[1,1,1,1]]), (5, array![[1], [1], [1], [1]])],
+    });
+
+    static ref O_PIECE: Arc<Piece> = Arc::new(Piece {
+        id: 1,
+        bgr: [2,159,227],
+        orientations: vec![(4, array![[1, 1], [1, 1]])],
+    });
+
+    static ref T_PIECE: Arc<Piece> = Arc::new(Piece {
+        id: 2,
+        bgr: [138, 41, 175],
+        orientations: vec![(3, array![[0, 1, 0], [1, 1, 1]]), (4, array![[1, 0], [1, 1], [1, 0]]), (3, array![[1, 1, 1], [0, 1, 0]]), (3, array![[0, 1], [1, 1], [0, 1]])],
+    });
+
+    static ref S_PIECE: Arc<Piece> = Arc::new(Piece {
+        id: 3,
+        bgr: [1, 177, 89],
+        orientations: vec![(3, array![[0, 1, 1], [1, 1, 0]]), (4, array![[1, 0], [1, 1], [0, 1]])],
+    });
+
+    static ref Z_PIECE: Arc<Piece> = Arc::new(Piece {
+        id: 4,
+        bgr: [55, 15, 215],
+        orientations: vec![(3, array![[1, 1, 0], [0, 1, 1]]), (4, array![[0, 1], [1, 1], [1, 0]])],
+    });
+
+    static ref J_PIECE: Arc<Piece> = Arc::new(Piece {
+        id: 5,
+        bgr: [198, 65, 33],
+        orientations: vec![(3, array![[1, 0, 0], [1, 1, 1]]), (4, array![[1, 1], [1, 0], [1, 0]]), (3, array![[1, 1, 1], [0, 0, 1]]), (3, array![[0, 1], [0, 1], [1, 1]])],
+    });
+
+    static ref L_PIECE: Arc<Piece> = Arc::new(Piece {
+        id: 6,
+        bgr: [2, 91, 227],
+        orientations: vec![(3, array![[0, 0, 1], [1, 1, 1]]), (4, array![[1, 0], [1, 0], [1, 1]]), (3, array![[1, 1, 1], [1, 0, 0]]), (3, array![[1, 1], [0, 1], [0, 1]])],
+    });
+    
+    //try adding a timer here and if it takes forever to run tetris_logic functions, that means it's doing this every time
+}
+
+// Evaluation Constants
+static mut BOX_UNIT: AtomicUsize = AtomicUsize::new(24);
+static mut HOLE_COST: AtomicI32 = AtomicI32::new(10);
+static mut HOLE_HEIGHT_COST: AtomicI32 = AtomicI32::new(2);
+static mut LINE_VALUE: AtomicI32 = AtomicI32::new(25);
+static mut JAGGED_COST: AtomicI32 = AtomicI32::new(2);
+static mut HEIGHT_THRESHOLD: AtomicU8 = AtomicU8::new(8);
+static mut HEIGHT_COST: AtomicI32 = AtomicI32::new(4);
+
+// Storing Pieces Constants
+static mut I_BLOCK: AtomicI32 = AtomicI32::new(2);
+static mut O_BLOCK: AtomicI32 = AtomicI32::new(0);
+static mut T_BLOCK: AtomicI32 = AtomicI32::new(1);
+static mut S_BLOCK: AtomicI32 = AtomicI32::new(0);
+static mut Z_BLOCK: AtomicI32 = AtomicI32::new(0);
+static mut J_BLOCK: AtomicI32 = AtomicI32::new(0);
+static mut L_BLOCK: AtomicI32 = AtomicI32::new(0);
+
+fn get_box_unit() -> usize {
+    unsafe {
+        BOX_UNIT.load(Ordering::Relaxed)
+    }
+}
+fn get_hole_cost() -> i32 {
+    unsafe {
+        HOLE_COST.load(Ordering::Relaxed)
+    }
+}
+fn get_hole_height_cost() -> i32 {
+    unsafe {
+        HOLE_HEIGHT_COST.load(Ordering::Relaxed)
+    }
+}
+fn get_line_value() -> i32 {
+    unsafe {
+        LINE_VALUE.load(Ordering::Relaxed)
+    }
+}
+fn get_jagged_cost() -> i32 {
+    unsafe {
+        JAGGED_COST.load(Ordering::Relaxed)
+    }
+}
+fn get_height_threshold() -> u8 {
+    unsafe {
+        HEIGHT_THRESHOLD.load(Ordering::Relaxed)
+    }
+}
+fn get_height_cost() -> i32 {
+    unsafe {
+        HEIGHT_COST.load(Ordering::Relaxed)
+    }
+}
+struct Field {
+    field_state: Array::<u8, Ix2>,
+    upcoming_pieces: [u8;5],
+    stored_piece: Option<u8>,
+    value: f32,
+    combo: u8,
+}
+
+impl Field {
+    fn new(upcoming_pieces: [u8;5]) -> Field {
+        Field {
+            field_state: Array::<u8, Ix2>::zeros((20, 10)),
+            // Create a three-dimensional f64 array, initialized with zeros
+            upcoming_pieces,
+            stored_piece: None,
+            value: 0.0,
+            combo: 0,
+        }
+    }
+    fn from<'a>(prior_field: &Field, new_move: &Move, new_piece: &Piece) -> Result<Field, &'a str> {
+        let tmp2;
+        let mut new_piece = new_piece;
+        let stored_piece = if new_move.store == true {
+            match prior_field.stored_piece {
+                Some(p) => {
+                    let tmp = new_piece.id;
+                    tmp2 = Piece::get_piece_from_id(p).unwrap().clone();
+                    new_piece = &tmp2;
+                    Some(tmp)
+                }
+                None => panic!("Smh. we are lazy and don't want to deal with a situation where there isn't a piece already stored xdddddd"),
+            }
+        } else {
+            prior_field.stored_piece.clone()
+        };
+        let mut f = Field {
+            //switches the stored piece if the move is "true" for store
+            stored_piece,
+             //But meme maybe change later
+            
+            field_state: {
+                let mut field_state = prior_field.field_state.clone();
+                let x_start = (new_move.position + new_piece.orientations[new_move.rotation as usize].0) as usize;
+                let piece_width = new_piece.orientations[new_move.rotation as usize].1.shape()[1];
+                let current_field = field_state.slice(s![0..20,  x_start..x_start + piece_width]);
+                //FOR LOOP METHOD
+                let mut max_block_y_value = 20;
+                for y in 0..20 {
+                    match current_field.slice(s![y, ..]).into_iter().find(|&&x| x == 1) {
+                        Some(v) => {
+                            max_block_y_value = y;
+                            break;
+                        },
+                        None => continue,
+                    };
+                }
+        
+                //ITER METHOD
+                /*let mut max_block_y_value = match current_field.into_iter().position(|&v| v == 1) {
+                    Some(index) => (index - index%piece_width)/piece_width,
+                    None => 20,
+                };*/
+        
+                let mut danger_height = false;
+                max_block_y_value = if max_block_y_value < new_piece.orientations[new_move.rotation as usize].1.shape()[0] {
+                    danger_height = true;
+                    new_piece.orientations[new_move.rotation as usize].1.shape()[0]
+                }
+                else {
+                    max_block_y_value
+                };
+                let mut found = false;
+                let piece_size = new_piece.orientations[new_move.rotation as usize].1.shape();
+                for y in max_block_y_value..20 + piece_size[0] {
+                    if y > 20 {
+                        break;
+                    }
+                    let test_field_section = &current_field.slice(s![y - piece_size[0]..y, ..]) + &new_piece.orientations[new_move.rotation as usize].1;
+                    match test_field_section.into_iter().find(|&&x| x == 2) {
+                        Some(_v) => {
+                            if danger_height {
+                                return Err("smh too high");
+                            }
+                            let mut tmp = field_state.slice_mut(s![y - 1 - piece_size[0]..y - 1, x_start..x_start + new_piece.orientations[new_move.rotation as usize].1.shape()[1]]);
+                            tmp += &new_piece.orientations[new_move.rotation as usize].1;
+                            found = true;
+                            break;
+                        },
+                        None => {
+                            danger_height = false;
+                            continue;
+                        },
+                    }
+                }
+                if !found {
+                    let mut tmp = field_state.slice_mut(s![20 - piece_size[0]..20, x_start..x_start + new_piece.orientations[new_move.rotation as usize].1.shape()[1]]);
+                    tmp += &new_piece.orientations[new_move.rotation as usize].1;
+                }
+                field_state
+            },
+            upcoming_pieces: prior_field.upcoming_pieces.clone(), 
+            
+
+            value: prior_field.value.clone(),
+            combo: prior_field.combo.clone(),
+        };
+        Field::clean_sent_lines(&mut f);
+        Ok(f)
+    }
+    fn eval_sent_lines(attack:usize) -> f32 {
+        match attack {
+            0 => 0.0, 
+            1 => -0.5, 
+            2 | 3 => (attack - 1).pow(2) as f32,
+            4 => 16.0,
+            _ => (attack).pow(2) as f32, 
+        }
+    }
+    fn clean_sent_lines(field: &mut Field){
+        let mut count = 0;
+        let blank = Array::<u8, Ix2>::zeros((1,10));
+        for y in 0..20 {
+            if field.field_state.slice(s![y, 0..10]).sum() == 10 {
+                count += 1;
+                field.field_state = stack![Axis(0), blank.view(), field.field_state.slice(s![0..y, 0..10]), field.field_state.slice(s![y + 1..20, 0..10])];
+            }
+        }
+        if (field.field_state.slice(s![19, 0..10])).sum() == 0 {
+            count = 10;
+        }
+        if count != 0 {
+            field.combo += 1;
+        }
+        else {
+            field.combo = 0;
+        }
+        field.value += get_line_value() as f32 * Field::eval_sent_lines(count) as f32;
+    }
+    fn eval(&self) -> f32 {
+        let mut y_values = [0; 10];
+        let mut height_costs = 0;
+        let mut hole_count = 0;
+        let mut hole_value = 0;
+        let state = &self.field_state;
+        let mut jaggedness = 0;
+        for x in 0..10 {
+            let mut height_found = false;
+            //Heights
+            match state.slice(s![.., x]).iter().position(|&y| y == 1) {
+                Some(index) => {
+                    height_found = true;
+                    //height_sum += 20 - index;
+                    y_values[x] = index;
+                    if (y_values[x] as u8) < (20 - get_height_threshold() as u8) {
+                        height_costs += 20 - y_values[x] - get_height_threshold() as usize;
+                    }
+                }
+                None => {
+                    y_values[x] = 20;
+                    continue
+                }
+            }
+            if x > 0 {
+                jaggedness += (y_values[x] as isize - y_values[x-1] as isize).pow(2);
+            }
+            //Holes
+            if height_found {
+                for y in y_values[x]..20 {
+                    if state[[y, x]] == 0 {
+                        hole_value += y - y_values[x];
+                        hole_count += 1;
+                    }
+                }
+            }
+        }
+        let score = -(get_jagged_cost() * jaggedness as i32 + get_hole_height_cost() * hole_value as i32 + get_hole_cost() * hole_count as i32 + get_height_cost() * height_costs as i32) as f32;
+        score
+    }
+    
+    fn calculate_all_resulting_fields(&self, depth: usize) -> Option<Move> {
+        if depth == 0 {
+            println!("{}",self.eval());
+            None
+        }
+        else {
+            let child_result = Arc::new(Mutex::new((0, f32::MIN)));
+            let piece = Piece::get_piece_from_id(self.upcoming_pieces[0]).unwrap();
+            let mut handles = Vec::new();
+            let mut moves = if let Some(stored_piece) = self.stored_piece {
+                let mut vec = piece.get_all_moves(false);
+                vec.append(&mut Piece::get_piece_from_id(stored_piece).unwrap().get_all_moves(true));
+                vec
+            }
+            else {
+                piece.get_all_moves(false)
+            };
+            let original_depth = depth;
+            for i in 0..moves.len() {
+                let field = match Field::from(self, &moves[i], &piece) {
+                    Ok(f) => f,
+                    Err(_e) => continue,
+                };
+                let result_clone = Arc::clone(&child_result);
+                let handle = thread::spawn(move || {
+                    recursive_resulting_fields(field, depth - 1, i, result_clone, original_depth);
+                });
+                handles.push(handle);
+            }        
+            for handle in handles {
+                handle.join().expect("heck I died lol");
+            }
+            let move_id = child_result.lock().unwrap().0;
+            Some(moves[move_id])
+        }
+    }
+}
+fn recursive_resulting_fields(f: Field, depth: usize, move_id: usize, result: Arc<Mutex<(usize, f32)>>, original_depth: usize) {
+    if depth == 0 {
+        let eval = f.eval();
+        let mut max_eval = result.lock().unwrap();
+        //println!("{},{}",move_id, eval);
+        //println!("{}", f);
+        if max_eval.1 < eval {
+            *max_eval = (move_id, eval);
+            //println!("{},{}",move_id, eval);
+        }
+    }
+    else {
+        let child_result = Arc::new(Mutex::new((0, f32::MIN)));
+        let piece = Piece::get_piece_from_id(f.upcoming_pieces[original_depth - depth]).unwrap();
+        let mut handles = Vec::new();
+        for m in if let Some(stored_piece) = f.stored_piece {
+            let mut vec = piece.get_all_moves(false);
+            vec.append(&mut Piece::get_piece_from_id(stored_piece).unwrap().get_all_moves(true));
+            vec
+        }
+        else {
+            piece.get_all_moves(false)
+        } {
+            let field = match Field::from(&f, &m, &piece) {
+                Ok(f) => f,
+                Err(_e) => continue,
+            };
+            let result_clone = Arc::clone(&child_result);
+            let handle = thread::spawn(move || {
+                recursive_resulting_fields(field, depth - 1, move_id, result_clone, original_depth);
+            });
+            handles.push(handle);
+        }
+        for handle in handles {
+            handle.join().unwrap();
+        }
+        let mut max_eval = result.lock().unwrap();
+        let child_result = child_result.lock().unwrap().1;
+        if max_eval.1 < child_result {
+            *max_eval = (move_id, child_result);
+        }
+    }
+}
+
+impl Display for Field {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let mut output = String::new();
+        for y in 0..20 {
+            for x in 0..10 {
+                output = format!("{} {}", output, self.field_state[[y,x]]);
+            }
+            output = format!("{}{}", output, "\n");
+        }
+        write!(f, "({}\nUpcoming Pieces: {:?}, Stored: {})", output, self.upcoming_pieces, match self.stored_piece {Some(p) => p.to_string(), None => String::from("N")})
+    }
+}
+
+#[derive(Copy, Clone)]
+struct Move {
+    rotation: u8,
+    position: i8,
+    store: bool,
+}
+
+impl Move {
+    fn from(m: (u8, i8, bool)) -> Move {
+        Move {
+            rotation: m.0,
+            position: m.1,
+            store: m.2,
+        }
+    }
+}
+
+impl Display for Move {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "(Rotation: {}, Position: {})", self.rotation, self.position)
+    }
+}
+
+#[derive(Clone)]
+struct Piece {
+    id: u8,
+    bgr: [u8;3],
+    //Orientation: how many blocks from left, matrix representing dimensions
+    orientations: Vec<(i8, Array2::<u8>)>,
+}
+
+impl Piece {
+    fn get_all_moves(&self, store: bool) -> Vec<Move> {
+        let mut moves = Vec::new();
+        for i in 0..self.orientations.len() {
+            for x in -self.orientations[i].0..(10 - self.orientations[i].1.shape()[1] as i8 - self.orientations[i].0 + 1) {
+                moves.push(Move {
+                    rotation: i as u8,
+                    position: x,
+                    store,
+                })
+            }
+        }
+        moves
+    }
+    fn get_piece_from_id<'a>(id: u8) -> Result<Arc<Piece>, &'a str> {
+        match id {
+            0 => Ok(Arc::clone(&I_PIECE)),
+            1 => Ok(Arc::clone(&O_PIECE)),
+            2 => Ok(Arc::clone(&T_PIECE)),
+            3 => Ok(Arc::clone(&S_PIECE)),
+            4 => Ok(Arc::clone(&Z_PIECE)),
+            5 => Ok(Arc::clone(&J_PIECE)),
+            6 => Ok(Arc::clone(&L_PIECE)),
+            _ => Err("Specified piece id does not exist!"),
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use ndarray::prelude::*;
+
+    use std::sync::Arc;
+    use std::thread;
+    use super::Field;
+    use super::Move;
+    use super::Piece;
+    use std::time;
+
+    #[test]
+    fn from_test() {
+        
+        let mut field = Field::new([1,2,6,5,4]);
+        let m = Move::from((0,0,true));
+        field.stored_piece = Some(0);
+        println!("{}", field);
+        field.calculate_all_resulting_fields(1);
+    }
+}
